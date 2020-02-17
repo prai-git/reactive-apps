808b8dc6c5a53ede5b62ba5557e1398b7734828a
Pankaj Rai <prai@gotprint.com>
2020-02-17 17:51:05 +0530

    create java-akka-cash-operation

---
+++ java-akka-cash-operations/src/test/java/com/prai/banking/SavingAccountTest.java
@@ -0,0 +1,42 @@
+package com.prai.banking;
+
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.testkit.javadsl.TestKit;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.prai.banking.SavingAccount;
+
+import static org.junit.Assert.assertEquals;
+
+public class SavingAccountTest {
+
+    static ActorSystem system;
+
+    @BeforeClass
+    public static void setup() {
+        system = ActorSystem.create();
+    }
+
+    @AfterClass
+    public static void teardown() {
+        TestKit.shutdownActorSystem(system);
+        system = null;
+    }
+
+
+    @Test
+    public void testCalculateInterestSavings () {
+        TestKit probe = new TestKit(system);
+
+        ActorRef accountActor = system.actorOf(SavingAccount.props(123, 100, 0.1f, 123));
+        accountActor.tell(new SavingAccount.CalculateInterestSavings(), probe.getRef());
+
+        assertEquals(10, probe.expectMsgClass(SavingAccount.ResponseInterestSavings.class).amount);
+
+
+    }
+
+}
